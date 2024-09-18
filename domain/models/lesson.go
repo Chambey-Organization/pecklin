13bd63853691590c2260d3a7e2f9f9c195b617ae
Charles Muchogo <muchpaul2@gmail.com>
2024-09-18 14:08:38 +0300

    Updated lessons model

--- domain/models/lesson.go
+++ domain/models/lesson.go
@@ -4,5 +4,6 @@ type Lesson struct {
 	ID         uint            `gorm:"primary_key;autoIncrement:true" json:"id"`
 	PracticeID uint            `json:"-"`
 	Title      string          `json:"lessonTitle"`
+	Active     bool            `json:"active"`
 	Content    []LessonContent `gorm:"foreignKey:LessonID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"content"`
 }
--- domain/models/lessonContent.go
+++ domain/models/lessonContent.go
@@ -4,4 +4,5 @@ type LessonContent struct {
 	ID       uint   `gorm:"primary_key;autoIncrement:true" json:"id"`
 	LessonID uint   `json:"-"`
 	Prompt   string `json:"prompt"`
+	Active   bool   `json:"active"`
 }
