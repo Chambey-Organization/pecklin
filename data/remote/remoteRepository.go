de90e4f4e366e34dd92780fcef84d60dc847c480
charles <muchpaul2@gmail.com>
2024-12-29 12:45:31 +0300

    Added failsafe for failed network calls

--- data/remote/remoteRepository.go
+++ data/remote/remoteRepository.go
@@ -7,30 +7,47 @@ import (
 	"main.go/data/local/database"
 	"main.go/domain/models"
 	"net/http"
+	"os"
 )
 
 func FetchPractices() error {
 
-	response, err := http.Get("https://www.charlesmuchogo.com/api/practice")
+	response, err := http.Get("http://13.244.41.201:8000/api/practice")
 	if err != nil {
-		return fmt.Errorf("failed to get response: %v", err)
+		return readPracticesFromFile()
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %v", response.StatusCode)
+		return readPracticesFromFile()
 	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return fmt.Errorf("failed to read response body: %v", err)
+		return readPracticesFromFile()
 	}
 
 	var getPracticeDTO models.GetPracticesDTO
 	if err := json.Unmarshal(responseData, &getPracticeDTO); err != nil {
-		return fmt.Errorf("failed to unmarshal JSON: %v", err)
+		return readPracticesFromFile()
 	}
 
 	database.InsertPractices(getPracticeDTO.Practices)
+	return nil
+}
+
+func readPracticesFromFile() error {
+	fileData, err := os.ReadFile("practices.json")
+	if err != nil {
+		return fmt.Errorf("failed to read practices.json: %v", err)
+	}
+
+	var getPracticeDTO models.GetPracticesDTO
+	if err := json.Unmarshal(fileData, &getPracticeDTO); err != nil {
+		return fmt.Errorf("failed to unmarshal JSON from practices.json: %v", err)
+	}
+
+	database.InsertPractices(getPracticeDTO.Practices)
+	fmt.Println("Successfully loaded practices from practices.json.")
 	return nil
 }
